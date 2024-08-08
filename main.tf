13131e6c6db9dd744a32a6bdb59d32fbb27ad846
MOM Lakki DAD <satheeshrayee@gmail.com>
2024-08-08 10:07:25 +0530

    Jfrog

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# Versions 
 terraform {
   required_providers {
     aws = {
@@ -7,11 +8,13 @@ terraform {
   }
 }
 
+# Authentication to AWS from Terraform code
 provider "aws" {
   region  = "us-east-1"
   profile = "default"
 }
 
+#Statefile storage 
 terraform {
   backend "s3" {
     bucket = "satheesh03011"
@@ -20,6 +23,7 @@ terraform {
   }
 }
 
+/* # Continuous Integration - Jenkins
 resource "aws_instance" "satheesh_Jenkins" {
   ami                    = var.ami
   instance_type          = var.instance_type
@@ -34,6 +38,7 @@ resource "aws_instance" "satheesh_Jenkins" {
   }
 }
 
+# Continuous Static Code Analysis Tool - SonarQube
 resource "aws_instance" "satheesh_Sonarqube" {
   ami                    = var.ami
   instance_type          = var.instance_type
@@ -46,4 +51,34 @@ resource "aws_instance" "satheesh_Sonarqube" {
     Name      = "sathiSonarqube"
     CreatedBy = "Terraforms"
   }
+} */
+
+# Continuous Binary Code Repository - JFROG
+resource "aws_instance" "sathi_jfrog" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = var.security_group_ids
+  iam_instance_profile   = var.iam_instance_profile
+  user_data              = file("jfrog.sh")
+  tags = {
+    Name      = "sathi_jfrog"
+    CreatedBy = "Terraform"
+  }
 }
+
+/* # Application Server - Apache Tomcat 
+resource "aws_instance" "sathi_tomcat" {
+  ami = var.ami
+  instance_type = var.instance_type
+  key_name = var.key_name
+  subnet_id = var.subnet_id
+  vpc_security_group_ids = var.security_group_ids
+  iam_instance_profile = var.iam_instance_profile
+  user_data = file("tomcat.sh")
+  tags = {
+    Name="sathi_tomacat1"
+    CreatedBy="terraform"
+  }
+} */
